a451fd331134864060a8e836504ccfc7e78863c9
salahmak <salah.mak@protonmail.com>
2021-02-13 19:35:19 +0100

    hided the console window in windows platform

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,16 @@
 #include "config.h"
 #include <iostream>
 
+#ifdef _WIN32
+	#include <windows.h>
+#endif
+
 int main()
 {
+#ifdef _WIN32
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+#endif
+
     GameWrapper::Game(SCREEN_WIDTH, SCREEN_HEIGHT, "Flappy Bird");
     return EXIT_SUCCESS;
 }
